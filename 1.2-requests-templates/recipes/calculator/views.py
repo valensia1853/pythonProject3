d9e886837a7003fb250857756d12e5e9f774d3d2
batsokin93 <152905867+valensia1853@users.noreply.github.com>
2024-08-25 00:32:06 +0300

    Update views.py

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -28,3 +28,19 @@ DATA = {
 #     'ингредиент2': количество2,
 #   }
 # }
+
+ef cook(request, recipe_name):
+    if recipe_name in DATA.keys():
+        servings = request.GET.get('servings', 1)
+        data = {}
+        for ingredient in DATA[recipe_name]:
+            data[ingredient] = round(DATA[recipe_name][ingredient] * int(servings), 2)
+
+        context = {
+            'recipe': data
+        }
+
+    else:
+        context = {}
+
+    return render(request, 'calculator/index.html', context)
